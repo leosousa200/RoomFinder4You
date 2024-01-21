e31e2cd461cfa286e29f47648de54a2dc5d3106f
leonardosousa <leodgsousa@gmail.com>
2024-01-21 13:10:37 +0000

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RoomFinder4You.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
